720564a1d53ff69c7e9383705968e46bf1afe248
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-16 21:28:11 -0500

    challenges complete

--- Lab03-Review/Program.cs
+++ Lab03-Review/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Lab03_Review
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Lab03_Review
 {
     public class Program
     {
@@ -8,6 +13,8 @@
             Challenge2();
             Challenge3();
             Challenge4();
+            Challenge5();
+            Challenge6();
         }
 
         public static int Challenge1()
@@ -112,6 +119,109 @@
                 }
             }
             Console.Write("Output: " + maxNumber);
+        }
+
+        public static int Challenge5()
+        {
+            Console.Write("Input an array of numbers:");
+            string? userInput = Console.ReadLine();
+            string[] inputArray = userInput.Split(" ");
+
+            int maxNum = int.MinValue; // Starting with the lowest possible integer value
+
+            foreach (string strNum in inputArray)
+            {
+                if (int.TryParse(strNum, out int value)) // Convert each string to an integer
+                {
+                    if (value > maxNum) // If the current number is larger than maxNum
+                    {
+                        maxNum = value; // Update maxNum to the current number
+                    }
+                }
+            }
+
+            Console.WriteLine("The maximum number in the array is: " + maxNum);
+            return maxNum;
+        }
+
+        public static void Challenge6()
+        {
+            Console.Write("Please input a word: ");
+            string word = Console.ReadLine();
+
+            SaveWordToFile(word);
+
+             static void SaveWordToFile(string word)
+            {
+                string filePath = "words.txt";
+
+                using (StreamWriter sw = File.AppendText(filePath))
+                {
+                    sw.WriteLine(word);
+                }
+
+                Console.WriteLine("Word saved to file successfully!");
+            }
+        }
+
+        public static void Challenge7()
+        {
+            string filePath = "words.txt"; 
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        public static void Challenge8()
+        {
+            RemoveSpecificWord("wordToRemove");
+
+            static void RemoveSpecificWord(string word)
+            {
+                string filePath = "words.txt"; 
+
+                string text = File.ReadAllText(filePath);
+
+                
+                string modifiedText = Regex.Replace(text, @"\b" + word + @"\b", "");
+
+                
+                File.WriteAllText(filePath, modifiedText);
+            }
+        }
+        public static void Challenge9()
+        {
+            Console.Write("Please input a sentence: ");
+            string sentence = Console.ReadLine();
+
+            string[] wordLengthArray = WordLengths(sentence);
+
+            Console.WriteLine("Output: ");
+            foreach (string wordLength in wordLengthArray)
+            {
+                Console.WriteLine(wordLength);
+            }
+
+
+             static string[] WordLengths(string sentence)
+            {
+                string[] words = sentence.Split(' ');
+
+                string[] wordLengths = new string[words.Length];
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    wordLengths[i] = $"{words[i]}: {words[i].Length}";
+                }
+
+                return wordLengths;
+            }
         }
     }
 }
